cb8ed04a4cdeab73f04ec0d5b81e2c153ce31a85
Ouroboros <ouroboros@users.noreply.github.com>
2026-04-04 01:59:26 +0300

    SECURITY: Remove files containing exposed SSH credentials (passwords, IPs)

--- tmp/check_v2ray_config.py
+++
@@ -1,8 +0,0 @@
-import paramiko
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('91.108.237.229', username='root', password='kYyA08DTsHxn1P', timeout=30)
-stdin, stdout, stderr = ssh.exec_command('cat /usr/local/etc/v2ray/config.json')
-print(stdout.read().decode())
-ssh.close()
